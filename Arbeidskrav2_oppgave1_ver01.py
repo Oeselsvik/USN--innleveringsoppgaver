c3351a2ca90a05cd2aa85095f7faa10dc5c9ae4d
Oeselsvik <oeselsvik@outlook.com>
2025-01-04 10:48:01 +0100

    Update Arbeidskrav2_oppgave1_ver01.py
    jeg fant ikke ut hvordan man sletter denne filen

--- Arbeidskrav2_oppgave1_ver01.py
+++ Arbeidskrav2_oppgave1_ver01.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 27 16:17:55 2024
-
-@author: oskar
-"""
-
+# IKKE bruk denne ved innsending
 #%% Trinn 1: Be brukeren oppgi fødselsåret
 # Bruk input-funksjonen til å hente fødselsår fra brukeren og konverter til heltall
 alder = int(input('Hvilket år er du født? '))
